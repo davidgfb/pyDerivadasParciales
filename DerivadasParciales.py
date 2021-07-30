6183091b80731df9efe749fa5175a9f49dbc7893
davidgfb <davidgracia99@gmail.com>
2021-07-30 21:18:57 +0200

    Update DerivadasParciales.py

--- DerivadasParciales.py
+++ DerivadasParciales.py
@@ -1,13 +1,39 @@
-def dz(z):
+def menosGradiente(z): # decreciente
     if z in ("x*y", "xy", "x *y", "x * y", "x* y"):
-        return ("(y, x)")
+        return ("(-y, -x, 0)")
 
     elif z in ("x+y", "x + y", "x+ y", "x +y"):
-        return ("(1, 1)")
-    
+        return ("(-1, -1, 0)")
+
 #PROBADOR
+'''
 z, z1 = "xy", "x+y"
 
-print(dz(z), "debe ser (y, x)\n\n",
+print(menosGradiente(z), "debe ser (-y, -x, 0)\n\n",
 
-      dz(z1), "debe ser (1, 1)")
+      menosGradiente(z1), "debe ser (-1, -1, 0)")
+'''
+
+def d(z, P):
+    menosGradienteZ = menosGradiente(z)
+    
+    esProducto = menosGradienteZ == "(-y, -x, 0)"
+    
+    if len(P) == 2:
+        x, y = P  
+        
+    elif len(P) == 3:
+        x, y, z = P
+        
+        if esProducto:
+            if not z == x*y:
+                z = x*y
+                print("z debe ser", z)
+              
+    if esProducto:
+        return (-y, -x, 0)
+    
+#PROBADOR
+z, P = "xy", (0, 0, 1)
+
+print(d(z, P), "debe ser (0, 0, 0)")
